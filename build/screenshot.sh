1746f5096f0f44ac78f99b5364b13bbd359bb88f
Yoshihide Jimbo <yjimbo@gmail.com>
2012-06-17 12:50:58 +0900

    add github and vibrant-ink themes

--- build/screenshot.sh
+++ build/screenshot.sh
@@ -1,4 +1,6 @@
 #/usr/bin/sh
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=vibrant-ink http://prettify.local:8000/vibrant-ink/preview.html
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=github http://prettify.local:8000/github/preview.html
 webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night http://prettify.local:8000/tomorrow-night/preview.html
 webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow http://prettify.local:8000/tomorrow/preview.html
 webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night-eighties http://prettify.local:8000/tomorrow-night-eighties/preview.html
