8741f3536081b9d4bbe8e9a2ce61fcc6c6009652
Yoshihide Jimbo <yjimbo@gmail.com>
2016-04-16 00:16:17 +0900

    Update demo site

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,7 +2,9 @@ function load() {
   var links = document.querySelectorAll('.js-download-theme');
   [].forEach.call(links, function(link) {
     link.addEventListener('click', function(event) {
-      ga('send', 'event', 'Download', 'click', link.dataset.themeId);
+      if (window.ga) {
+        ga('send', 'event', 'Download', 'click', link.dataset.themeId);
+      }
     });
   });
 }
